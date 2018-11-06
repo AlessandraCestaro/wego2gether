6569a6df7802ec4e71c59a10cb3c19a4113a3513
Cristina Simoncini <crisimonc@gmail.com>
2018-11-06 15:49:27 +0100

    Seed: users, trips, destinations, usertrips

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :user_trips, dependent: :destroy
+  has_many :trips, through: :user_trips
+  has_many :managed_trips, foreign_key: "user_id", class_name: "Trip"
+  has_many :votes, dependent: :destroy
+
 end
