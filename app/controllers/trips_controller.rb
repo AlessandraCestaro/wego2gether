b0a5a543162d4314ed77eaeb92b1ec2618df425f
Alessandra Cestaro <alessandra_cestaro@mckinsey.com>
2018-11-08 11:23:00 +0100

    Trips dashboard complete, also for different user statuses

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,26 +1,18 @@
 class TripsController < ApplicationController
-	
+
 	def index
 		# DASHBOARD: ALL TRIPS
-		# managed:
-		# current_user.managed_trips.each do |trip|
-		# end
-
-		# invited:
-		# @user.trips.each do |trip|
-		 # if trip.user_id != current_user
-		  # display
-		#end
+    # @user = User.find(params[:user_id])
 	end
 
 	def show
-		#TRIP STATUS
+		#TRIP STATUS (depending on whether I have voted already)
 		#show members (accepted, declined, pending, voted)
 		#show final destination when available or time is out
 	end
 
 	def new
-		#group leader creates a trip and 
+		#group leader creates a trip and
 		#select name and destinations
 	end
 
