3179ccc7ff0574cc922e17d4754f5975d00ff48a
Cristina Simoncini <crisimonc@gmail.com>
2018-11-12 15:36:06 +0100

    wip

---
+++ app/assets/javascripts/places.js
@@ -0,0 +1,9 @@
+/*places.js*/
+
+var options = {
+  appId: 'plC8UYAMHK2J',
+  apiKey: '1269f51d6f6aceff6a532f29caa6fdb0',
+  container: document.querySelector('#search'),
+  // ...
+};
+places(options);
