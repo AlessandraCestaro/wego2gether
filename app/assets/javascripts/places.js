26c1d66d1d495e7810f41bafec5ef51cc4ded30d
Cristina Simoncini <crisimonc@gmail.com>
2018-11-12 18:46:56 +0100

    Algolia autocomplete and sweetalert

--- app/assets/javascripts/places.js
+++ app/assets/javascripts/places.js
@@ -1,9 +1,10 @@
 /*places.js*/
 
-var options = {
+/*var options = {
   appId: 'plC8UYAMHK2J',
   apiKey: '1269f51d6f6aceff6a532f29caa6fdb0',
   container: document.querySelector('#search'),
   // ...
 };
 places(options);
+*/
